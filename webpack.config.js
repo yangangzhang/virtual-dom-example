385fa2568d04ae402f636a6aeb1582f969d37592
yangangzhang <913800916@qq.com>
2019-10-31 16:25:32 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,73 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractCSS = new ExtractTextPlugin('css/[name]-css.css');
+const extractSASS = new ExtractTextPlugin('css/[name]-sass.css');
+//构建前删除dist目录
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
 module.exports = {
-    entry: path.join(__dirname, "/src/virtual-dom.js"), // 入口文件
-    output: {
-        path: path.join( __dirname, "/dist"), //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
-    }
+  entry: './src/virtual-dom.js', //入口JS
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: extractCSS.extract({
+          use: "css-loader",
+          fallback: "style-loader"
+        })
+
+
+      },
+      {
+        test: /\.scss$/,
+        use: extractSASS.extract({
+          use: [{
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true //缓存
+          }
+        }
+      },
+      { //打包css里的图片
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192, //小于8KB,就base64编码
+            name: 'img/[name].[ext]', //在哪里生成
+            publicPath: '../' //在生成的文件引用,前面加
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './example/index.html', // 模板文件          
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(['dist', 'build'], {
+      verbose: false,
+    })
+  ]
+
 }
